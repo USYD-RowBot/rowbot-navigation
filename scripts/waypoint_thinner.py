372e4bb13a3cb4aec9630bada098c6c7dc464e10
John Sumskas <john.sumskas@gmail.com>
2018-12-12 08:13:56 -1000

    Added thinning waypoint thinner

--- scripts/waypoint_thinner.py
+++ scripts/waypoint_thinner.py
@@ -15,12 +15,10 @@ def pathCallback(path_msg):
     for pose_stamped in path_msg.poses:
         pose = pose_stamped.pose
 
-        distance_to_last_pose = math.sqrt((pose.position.x - last_pose.position.x)^2 + (pose.position.y - last_pose.position.y)^2)
 
 
 
-        angle_to_last_pose = math.abs(tf.transformations.euler_from_quaternion(last_pose.orientation)[2] - tf.transformations.euler_from_quaternion(pose.orientation)[2])
-        if (angle_to_last_pose > 30 or distance_to_last_pose > 5 or count = len(path_msg.poses)):
+        if count%2 == 0:
             thinned.append(pose_stamped)
             last_pose = pose
 
