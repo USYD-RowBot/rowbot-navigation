c0663d8b35fe2b491a2b9dfce97210aab82fa5ab
John Sumskas <john.sumskas@gmail.com>
2018-12-12 11:05:13 -1000

    configured costmaps

--- scripts/waypoint_thinner.py
+++ scripts/waypoint_thinner.py
@@ -1,38 +1,42 @@
 #!/usr/bin/env python
 import rospy
 from nav_msgs.msg import Path
-from geometry_msgs.msgs import Pose
+from geometry_msgs.msg import Pose
 import math
 from tf.transformations import euler_from_quaternion
 
+class WaypointThinner:
+    def __init__(self):
+        self.pub_ = rospy.Publisher("waypoints", Path, queue_size=1)
+        rospy.Subscriber("/waypoints_full", Path, self.pathCallback)
+
+    def pathCallback(self,path_msg):
+        thinned_msg = Path()
+        thinned = []
+        last_pose = Pose()
+        count = 0
+        for pose_stamped in path_msg.poses:
+            pose = pose_stamped.pose
+            
 
 
-def pathCallback(path_msg):
-    thinned_msg = Path()
-    thinned = []
-    last_pose = Pose()
-    count = 0
-    for pose_stamped in path_msg.poses:
-        pose = pose_stamped.pose
 
 
+            if count%4 == 0:
+                thinned.append(pose_stamped)
+                last_pose = pose
 
+            count = count+1
 
-        if count%2 == 0:
-            thinned.append(pose_stamped)
-            last_pose = pose
-
-        count = count+1
-
-    thinned_msg.poses = thinned
-    thinned_msg.header.stamp = rospy.Time.now()
-    pub.publish(thinned_msg)
+        thinned_msg.poses = thinned
+        thinned_msg.header.stamp = rospy.Time.now()
+        thinned_msg.header.frame_id = "map"
+        self.pub_.publish(thinned_msg)
 
 
 
 
 if __name__ == "__main__":
     rospy.init_node('paththinner')
-
-    rospy.Subscriber("/waypoints_full",Path,callback)
-    pub = rospy.Publisher("waypoints")
+    wt = WaypointThinner()
+    rospy.spin()
